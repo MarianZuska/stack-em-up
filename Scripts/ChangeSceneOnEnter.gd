1f7f8aa63cfa313122a1e660b5e6f4ae0798a65a
LiquidFun <brutenis@gmail.com>
2021-02-15 21:29:42 +0100

    Spikes now scene, added level

---
+++ Scripts/ChangeSceneOnEnter.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var has_triggered = false
+
+export(NodePath) var next_level = null
+
+func _physics_process(delta):
+	if not has_triggered:
+		var bodies = get_overlapping_bodies()
+		for body in bodies:
+			if body.name == "Player":
+				has_triggered = true
+				if next_level == null:
+					next_level = get_tree().current_scene.filename
+				SceneChanger.change_scene(next_level, 0)
